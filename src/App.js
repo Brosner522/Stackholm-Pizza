5a5793b68f9884b073be924367a5083b1f1a3e51
Brosner522 <rosnerbb@gmail.com>
2021-06-01 12:01:21 -0400

    setting up connection

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import Navbar from "./Component/Navbar";
+import HomePage from "./Component/HomePage";
+import React, { Component } from "react";
+
+class App extends Component { 
+
+  state = {
+    pizzas: {},
+    customers: {},
+    orders: {},
+    toppings:{}
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:9292/pizzas")
+    .then(res => res.json())
+    .then(pizzas => this.setState({pizzas}))
+
+ 
+  
+  }
+
+  render() {
+    console.log("here")
+    return (
+      <div className="App">
+          <Navbar /> 
+          <HomePage pizzas={this.state.pizzas} customers={this.state.customers} orders={this.state.orders} toppings={this.state.toppings} />
+      </div>
+    );
+  }
+
 }
 
 export default App;
+
---
+++ src/Component/HomePage.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Pizza from "./Pizza"
+export default class HomePage extends Component { 
+
+render() {
+    return(
+        <div>
+            {/* {console.log(this.props.pizzas)} */}
+        </div>
+    )
+}
+}
+
+
+
